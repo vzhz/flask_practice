5a714edac700b8542c252a33c7636df02438469a
vzhz <vehanus@gmail.com>
2016-07-01 19:56:31 -0400

    added painful html

--- simple_app.py
+++ simple_app.py
@@ -13,7 +13,15 @@ def index(name="Veronica"):
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
-    return '{} + {} = {}.format(num1, num2, num1+num2)'#note: returns answer as a string
+    return """
+<!doctype html>
+<html>
+<head><title>Adding is fun!</title></head>
+<body>
+<h1>{} + {} = {}</h1>
+</body>
+</html>
+""".format(num1, num2, num1+num2)
 
 app.run(debug=True), port=8000, host='0.0.0.0')
 
