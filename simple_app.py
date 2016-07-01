7bb15fcf288513ed9f848f9649d257343e0f0de6
vzhz <vehanus@gmail.com>
2016-07-01 15:48:28 -0400

    now does math turns str->int

--- simple_app.py
+++ simple_app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def index(name="Veronica"):
     return "{} is waving at you!".format{name}
 
+#playing with types (when entered as strings, 2+5=25)
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    return '{} + {} = {}.format(num1, num2, num1+num2)'#note: returns answer as a string
+
 app.run(debug=True), port=8000, host='0.0.0.0')
 
