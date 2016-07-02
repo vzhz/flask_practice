b2676b1bacfaf8ee1b8dedc631e11f22cd03ac0c
vzhz <vehanus@gmail.com>
2016-07-01 21:22:59 -0400

    added add view template context

--- simple_app.py
+++ simple_app.py
@@ -13,15 +13,8 @@ def index(name="Veronica"):
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
-    return """
-<!doctype html>
-<html>
-<head><title>Adding is fun!</title></head>
-<body>
-<h1>{} + {} = {}</h1>
-</body>
-</html>
-""".format(num1, num2, num1+num2)
+    context = {'num1' : num1, 'num2' : num2}
+    return render_template('add.html', **context)
 
 app.run(debug=True), port=8000, host='0.0.0.0')
 
